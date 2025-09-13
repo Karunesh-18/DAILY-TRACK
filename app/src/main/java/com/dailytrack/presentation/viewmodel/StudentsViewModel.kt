815392089ea86e66aad805978e15489619010527
Karunesh <karunesh.ar2024cse@sece.ac.in>
2025-09-13 22:29:23 +0530

    Remove replaceAllWithBulkList function from StudentsViewModel

--- app/src/main/java/com/dailytrack/presentation/viewmodel/StudentsViewModel.kt
+++ app/src/main/java/com/dailytrack/presentation/viewmodel/StudentsViewModel.kt
@@ -1,10 +1,3 @@
-    // Admin action: replace all students with bulk list
-    fun replaceAllWithBulkList(onComplete: (() -> Unit)? = null) {
-        viewModelScope.launch {
-            studentRepository.replaceAllStudents(StudentRepository.bulkStudentList)
-            onComplete?.invoke()
-        }
-    }
 package com.dailytrack.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
